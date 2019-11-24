691a096e3226c1fd445681e8eddb063537a139e9
SorinGabriel02 <lupu_sg@yahoo.com>
2019-11-24 23:53:41 +0200

    change in the projects section

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,28 +5,29 @@ import project3 from "../assets/project3.png";
 const PROJECTS = [
   {
     id: 1,
+    title: "SoulLink",
+    description:
+      "An app that will let you connect with your friends, so you can post and chat. To build this app I used HTML CSS and JavaScript on the frontend and Node Express and MongoDB for the backend.",
+    linkCode: "https://github.com/SorinGabriel02/sLink",
+    linkLive: "https://soul-link.herokuapp.com/",
+    image: project1
+  },
+  {
+    id: 2,
     title: "Ravenous",
     description:
       "React APP that is using the Yelp API and lets the user look for a type of business in a certain area.",
     linkCode: "http://github.com/SorinGabriel02/raven",
     linkLive: "https://soringabriel02.github.io/raven/",
-    image: project1
+    image: project2
   },
   {
-    id: 2,
+    id: 3,
     title: "Jahming",
     description:
       "React web app that uses the Spotify API and lets the user search for tunes and create a Spotify playlist.",
     linkCode: "http://github.com/SorinGabriel02/jahming",
     linkLive: "http://jahming.surge.sh/",
-    image: project2
-  },
-  {
-    id: 3,
-    title: "Web site",
-    description: "Updating soon...",
-    linkCode: "#",
-    linkLive: "#",
     image: project3
   }
 ];
